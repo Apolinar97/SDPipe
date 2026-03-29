5cd667bcd80ece44df2c0a09c96f0da19c976cee
camacho.apolinar97@gmail.com <camacho.apolinar97@gmail.com>
2026-03-29 15:15:45 -0700

    Feature: Added NWS fact and dim schema to marts layer

---
+++ sdpipe_transforms/models/marts/dim_weather_station.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='table', unique_key='station_key') }}
+
+WITH latest_per_station AS (
+    SELECT DISTINCT ON (station_id)
+        station_id,
+        station_name,
+        latitude,
+        longitude
+    FROM {{ ref('int_nws_observations') }}
+    ORDER BY station_id, observation_timestamp DESC
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['station_id']) }} AS station_key,
+    station_id,
+    station_name,
+    latitude,
+    longitude
+FROM latest_per_station
