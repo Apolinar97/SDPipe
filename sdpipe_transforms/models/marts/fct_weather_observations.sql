9c67a6a6ee802be830d84504794dee3d9ca9d018
camacho.apolinar97@gmail.com <camacho.apolinar97@gmail.com>
2026-03-29 15:15:45 -0700

    Feature: Added NWS fact and dim schema to marts layer

---
+++ sdpipe_transforms/models/marts/fct_weather_observations.sql
@@ -0,0 +1,24 @@
+{{ config(unique_key='observation_id') }}
+
+WITH obs AS (
+    SELECT * FROM {{ ref('int_nws_observations') }}
+)
+
+SELECT
+    observation_id,
+    {{ dbt_utils.generate_surrogate_key(['station_id']) }} AS station_key,
+    CAST(TO_CHAR(observation_date, 'YYYYMMDD') AS INTEGER) AS date_key,
+    observation_timestamp,
+    observation_hour,
+    temperature_c,
+    wind_direction_deg,
+    wind_speed_kmh,
+    wind_gust_kmh,
+    visibility_m,
+    precip_last_1h_mm,
+    precip_last_3h_mm,
+    precip_last_6h_mm,
+    cloud_cover,
+    cloud_base_m,
+    text_description
+FROM obs
