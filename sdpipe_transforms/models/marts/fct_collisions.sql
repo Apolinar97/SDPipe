6a6e9f06a97cb4f26fcb1e2ccf7898464b1cfd44
camacho.apolinar97@gmail.com <camacho.apolinar97@gmail.com>
2026-04-03 14:00:04 -0700

    Refactor/Fix: Added weather columns to fct_collisions, moved logic into fct logic as CTEs)

--- sdpipe_transforms/models/marts/fct_collisions.sql
+++ sdpipe_transforms/models/marts/fct_collisions.sql
@@ -10,6 +10,40 @@ participant_counts AS (
         COUNT(*) AS participant_count
     FROM {{ ref('int_collisions_details') }}
     GROUP BY report_id
+),
+
+collision_stations AS (
+    SELECT
+        b.report_id,
+        b.report_hour,
+        CAST(TO_CHAR(b.report_date, 'YYYYMMDD') AS INTEGER) AS date_key,
+        bs.station_key,
+        bs.distance_to_station_km
+    FROM {{ ref('int_collisions_basic') }} AS b
+    LEFT JOIN {{ ref('map_beat_station') }} AS bs
+        ON {{ dbt_utils.generate_surrogate_key(['b.police_beat']) }} = bs.beat_key
+),
+
+hourly_weather AS (
+    SELECT DISTINCT ON (station_key, date_key, observation_hour_local)
+        observation_id,
+        station_key,
+        date_key,
+        observation_hour_local,
+        observation_timestamp,
+        temperature_c,
+        wind_direction_deg,
+        wind_speed_kmh,
+        wind_gust_kmh,
+        visibility_m,
+        precip_last_1h_mm,
+        precip_last_3h_mm,
+        precip_last_6h_mm,
+        cloud_cover,
+        cloud_base_m,
+        text_description
+    FROM {{ ref('fct_weather_observations') }}
+    ORDER BY station_key, date_key, observation_hour_local, observation_timestamp
 )
 
 SELECT
@@ -42,7 +76,29 @@ SELECT
     b.is_fatal,
     b.has_injuries,
     b.is_hit_and_run,
-    b.is_intersection_collision
+    b.is_intersection_collision,
+    cs.station_key,
+    cs.distance_to_station_km,
+    hw.observation_id,
+    hw.observation_hour_local,
+    hw.temperature_c,
+    hw.wind_direction_deg,
+    hw.wind_speed_kmh,
+    hw.wind_gust_kmh,
+    hw.visibility_m,
+    hw.precip_last_1h_mm,
+    hw.precip_last_3h_mm,
+    hw.precip_last_6h_mm,
+    hw.cloud_cover,
+    hw.cloud_base_m,
+    hw.text_description AS weather_description
 FROM basic AS b
 LEFT JOIN participant_counts AS pc
     ON b.report_id = pc.report_id
+LEFT JOIN collision_stations AS cs
+    ON b.report_id = cs.report_id
+LEFT JOIN hourly_weather AS hw
+    ON
+        cs.station_key = hw.station_key
+        AND cs.date_key = hw.date_key
+        AND b.report_hour = hw.observation_hour_local
